962caae5a2fd69739e1a46937c90c71f223b9067
hammadfozi <hammadfozi@gmail.com>
2019-08-24 23:11:14 +0500

    Scraped to get download links, entered testing phase

--- main.py
+++ main.py
@@ -9,25 +9,30 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 
+save_loc = "G:/KSD/"
 
 class DownloadManager:
     def __init__(self):
         self.q = queue.Queue(maxsize=20)
+        self.is_serving = False
 
     def add(self, dl_job):
         self.q.put(dl_job)
 
     def serve(self):
-        dl_job = self.q.get()
-        print("Starting")
+        if not self.is_serving:
+            self.is_serving = True
+            dl_job = self.q.get()
+            print("Starting")
 
-        obj = SmartDL(dl_job["url"], dest=dl_job["dest"])
-        obj.start(blocking=False)
-        # path = obj.get_dest()
-        while not obj.isFinished():
-            print("Speed: %s" % obj.get_speed(human=True))
-            print("Progress: %s" % (obj.get_progress() * 100))
+            obj = SmartDL(dl_job[3][0], dest=dl_job[-1])
+            obj.start(blocking=False)
+            # path = obj.get_dest()
+            while not obj.isFinished():
+                print("Speed: %s" % obj.get_speed(human=True))
+                print("Progress: %s" % (obj.get_progress() * 100))
 
+            self.is_serving = False
             # if obj.isSuccessful():
             #     print("DOWNLOADED SUCCESSFULLY!!")
             # else:
@@ -50,10 +55,10 @@ class DownloadManagerThread(threading.Thread):
 
 
 if __name__ == "__main__":
-    # download_manager = DownloadManager()
-    # dm_thread = DownloadManagerThread()
-    # dm_thread.start()
-    #
+    download_manager = DownloadManager()
+    dm_thread = DownloadManagerThread()
+    dm_thread.start()
+
     # dl1 = {
     #     "url": "https://www79.playercdn.net/p-dl/1/eG6Wu8AuddMtZpR6PAlvQA/1566566286/190822/086G686J2D1XPTX5SCINB.mp4?name=FateStayNightHeavensFeelIILostButterflyBDRip1920x1080x264FLAC-rh-332.mp4-1080.mp4",
     #     "dest": "D:/"
@@ -66,17 +71,76 @@ if __name__ == "__main__":
     driver = webdriver.Chrome("C:/chromedriver.exe")
     driver.get("https://kissanime.ru/Anime/Dr-Stone")
 
+    print("Extracting...")
     #episodes = driver.find_element_by_class_name("listing").find_element_by_tag_name("tbody").find_elements_by_tag_name("tr")
-    wait = WebDriverWait(driver, 10).until(
-         EC.visibility_of_element_located((By.XPATH, "/html/body/div/div[3]/div/div/ul/li/a"))
-      )
+    # wait = WebDriverWait(driver, 10).until(
+    #      EC.visibility_of_element_located((By.XPATH, "/html/body/div/div[3]/div/div/ul/li/a"))
+    #   )
 
-    print("YES")
-    print(driver.find_element_by_xpath("/html/body/div/div[4]/div/div[2]/div[2]/div[2]/table").text)
-    # episodes = driver.find_element_by_xpath("/html/body/div/div[4]/div/div[3]/div[2]/div[2]/table")
+    try:
+        wait = WebDriverWait(driver, 10).until(
+         EC.text_to_be_present_in_element((By.XPATH, "/html/body/div/div[3]/div/div/ul/li/a"), "Home")
+        )
+    except NoSuchElementException:
+        print("not Found")
+
+    episodes = driver.find_elements_by_xpath("/html/body/div/div[4]/div/div[2]/div[2]/div[2]/table/tbody/tr")[2:]
+    episode_list = []
+
+    for episode in reversed(episodes):
+        elem = episode.find_element_by_tag_name("td").find_element_by_tag_name("a")
+        #print(elem.text)
+        #print(elem.get_attribute("href"))
+        ep = [elem.text, elem.get_attribute("href")]
+        episode_list.append(ep)
+
+    for episode in episode_list:
+        # print(episode)
+        driver.get(episode[1] + "&s=rapidvideo")
+
+        path = driver.find_element_by_xpath("/html/body/div/div[4]/div/div/div/div[13]/div[2]/div/iframe").get_attribute("src")
+        index = path.rfind('/', 0, len(path)-1) + 1
+        path = "https://www.rapidvideo.com/d/" + path[index:]
+        episode.append(path)
+
+        driver.get(path)
+        #qualities = driver.find_elements_by_xpath("/html/body/div/section/section/div/div/p[2]/a")
+        qualities = driver.find_element_by_class_name("hero-body").find_elements_by_class_name("title")[1].find_elements_by_tag_name("a")
+
+        # 480, 720, 1080
+
+        quality_links = ["", "", ""]
+
+        for quality in qualities:
+            # print(quality.text)
+            # print(quality.get_attribute("href"))
+            if quality.text == "Download 480p":
+                quality_links[0] = quality.get_attribute("href")
+            elif quality.text == "Download 720p":
+                quality_links[1] = quality.get_attribute("href")
+            elif quality.text == "Download 1080p":
+                quality_links[2] = quality.get_attribute("href")
+            else:
+                print("UNKNOWN QUALITY: " + quality.text)
+
+        episode.append(quality_links)
+
+        episode.append(save_loc + str(episode[0]).replace(' ', '_') + ".mp4")
+
+        print(episode)
+        download_manager.add(episode)
+    print("Done")
+    # .get_attribute("src")
+
+    # try:
+    #     wait = WebDriverWait(driver, 10).until(
+    #      EC.text_to_be_present_in_element((By.XPATH, "/html/body/div/div[3]/div/div/ul/li/a"), "Home")
+    #     )
+    # except NoSuchElementException:
+    #     print("not Found")
     #
-    # #print(episodes.text)
-    #
-    # for episode in episodes:
-    #    print(episode.text)
+    # driver.find_element_by_xpath("/html/body/div/div[4]/div/div[2]/form/div[3]/")
+
+
+
 
