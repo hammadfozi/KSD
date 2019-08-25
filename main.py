cf775f43ca6fae4c6d2aa2a5ef1a7e2b0ee08e75
hammadfozi <hammadfozi@gmail.com>
2019-08-25 22:53:14 +0500

    - Added Kivy Library - Fixed Iframe sometimes not  found issue

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 save_loc = "G:/KSD/"
 
+
 class DownloadManager:
     def __init__(self):
         self.q = queue.Queue(maxsize=20)
@@ -56,35 +57,19 @@ class DownloadManagerThread(threading.Thread):
             if not download_manager.q.empty():
                 download_manager.serve()
             else:
-                print("Download Manager: Empty Queue")
+                #print("Download Manager: Empty Queue")
                 time.sleep(1)
 
 
-if __name__ == "__main__":
-    download_manager = DownloadManager()
-    dm_thread = DownloadManagerThread()
-    dm_thread.start()
-
-    # dl1 = {
-    #     "url": "https://www79.playercdn.net/p-dl/1/eG6Wu8AuddMtZpR6PAlvQA/1566566286/190822/086G686J2D1XPTX5SCINB.mp4?name=FateStayNightHeavensFeelIILostButterflyBDRip1920x1080x264FLAC-rh-332.mp4-1080.mp4",
-    #     "dest": "D:/"
-    # }
-    #
-    # download_manager.add(dl1)
-    #
-    # print("Ended")
-
-
-
-
+def scrape_urls(url):
     driver = webdriver.Chrome("C:/chromedriver.exe")
-    driver.get("https://kissanime.ru/Anime/Dr-Stone")
+    driver.get(url)
 
     print("Extracting...")
 
     try:
         wait = WebDriverWait(driver, 10).until(
-         EC.text_to_be_present_in_element((By.XPATH, "/html/body/div/div[3]/div/div/ul/li/a"), "Home")
+            EC.text_to_be_present_in_element((By.XPATH, "/html/body/div/div[3]/div/div/ul/li/a"), "Home")
         )
     except NoSuchElementException:
         print("not Found")
@@ -100,13 +85,15 @@ if __name__ == "__main__":
     for episode in episode_list:
         driver.get(episode[1] + "&s=rapidvideo")
 
-        path = driver.find_element_by_xpath("/html/body/div/div[4]/div/div/div/div[13]/div[2]/div/iframe").get_attribute("src")
-        index = path.rfind('/', 0, len(path)-1) + 1
+        path = driver.find_element_by_id("divMyVideo").find_element_by_tag_name("iframe").get_attribute("src")
+
+        index = path.rfind('/', 0, len(path) - 1) + 1
         path = "https://www.rapidvideo.com/d/" + path[index:]
         episode.append(path)
 
         driver.get(path)
-        qualities = driver.find_element_by_class_name("hero-body").find_elements_by_class_name("title")[1].find_elements_by_tag_name("a")
+        qualities = driver.find_element_by_class_name("hero-body").find_elements_by_class_name("title")[
+            1].find_elements_by_tag_name("a")
 
         # 480, 720, 1080
         quality_links = ["", "", ""]
@@ -125,19 +112,18 @@ if __name__ == "__main__":
         episode.append(save_loc + str(episode[0]).replace(' ', '_') + ".mp4")
 
         print(episode)
-        download_manager.add(episode)
-
-    # dl_1 = ["G:/KSD/Dr.Stone1.mp4", "", "", ["https://www223.playercdn.net/p-dl/2/vpuBQcU01kng-65ZWfO1Tw/1566773134/190705/688G4PDTCQK7UKWFFXDNT.mp4?name=HorribleSubsDr.Stone01720p-rh-62.mp4-480.mp4"]]
-    # download_manager.add(dl_1)
-    #
-    # time.sleep(10)
-    #
-    # dl_2 = ["G:/KSD/Dr.Stone2.mp4", "", "", ["https://www531.playercdn.net/p-dl/0/l1uFFv26QnycwnsOO-NNqQ/1566773990/190712/680G4XGU1I2Y7JFECFLNK.mp4?name=HorribleSubsDr.Stone02720p-rh-406.mp4-480.mp4"]]
-    # download_manager.add(dl_2)
-
     driver.close()
+    return episode_list
+
+
+if __name__ == "__main__":
+    # download_manager = DownloadManager()
+    # dm_thread = DownloadManagerThread()
+    # dm_thread.start()
+    #
+    # data = scrape_urls("https://kissanime.ru/Anime/Dr-Stone")
+    #
+    # for item in data:
+    #     download_manager.add(item)
 
     print("Done")
-
-
-
